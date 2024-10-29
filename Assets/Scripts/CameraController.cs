3927f8ae35ab5557e7d5066632e8a2b8250a78c8
Chimas <50328145+igor-saadeh@users.noreply.github.com>
2024-10-28 21:42:43 -0300

    Receba

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public PlayerController playerController; // temporario
-    //private float speed = 7f;
+    //private Camera cameraPlayer;
+    private float speed = 1f; // 7?
 
+    private void Start()
+    {
+        //Camera[] temp = GetComponentsInChildren<Camera>();
+
+        //foreach (Camera cam in temp)
+        //{
+        //    if (cam.gameObject.name == "Camera")
+        //    {
+        //        cameraPlayer = cam;
+        //        break;
+        //    }
+        //}    
+        //transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0); //rever
+        
+        float xPos = playerController.transform.position.x + 1; //prototipo
+        transform.position = new Vector3(xPos, playerController.transform.position.y, transform.position.z); //prototipo
+
+        //cameraPlayer.transform.position = transform.position;
+    }
 
     private void Update()
     {
-        //transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0); //rever
-        float xPos = playerController.transform.position.x + 1; //prototipo
-        
-        transform.position = new Vector3(xPos, playerController.transform.position.y, transform.position.z); //prototipo
+        transform.position += new Vector3(speed * Time.deltaTime, playerController.transform.position.y, 0);
     }
 }
+
+/*
+ * Camera se movimente no eixo X numa velocidade fixa (5f)
+ * Camera deve seguir o movimento do personagem em y
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
